2e6b4199fe2db0c8bbdf163545e356d7a7fc8a4d
Brett Davis <brett.p.davis101@gmail.com>
2020-08-11 22:27:23 +0000

    Adds db compatability to city and state TODO: verify city getter on state works as intended

--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.__init__ import storage
+from os import environ
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if environ['HBNB_TYPE_STORAGE'] == 'db':
+        cities = relationship("City", cascade="all,delete,delete-orphan",
+                              backref="state")
+
+    elif environ['HBNB_TYPE_STORAGE'] == 'file':
+        def cities(self):
+            return [{x: y} for x, y in storage._FileStorage__objects.items()
+                    if 'City' in x and y.state_id == self.id]

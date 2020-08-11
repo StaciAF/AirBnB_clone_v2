d899f0e7122839331fae135e39b554e918210d16
Brett Davis <brett.p.davis101@gmail.com>
2020-08-11 22:47:57 +0000

    Minor fixes to output of state cities getter TODO: weird issue where stuff isn't saving to a file, nto sure whats causing it

--- models/state.py
+++ models/state.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.__init__ import storage
-from os import environ
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    if environ['HBNB_TYPE_STORAGE'] == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship("City", cascade="all,delete,delete-orphan",
                               backref="state")
 
-    elif environ['HBNB_TYPE_STORAGE'] == 'file':
+    else:
+        @property
         def cities(self):
-            return [{x: y} for x, y in storage._FileStorage__objects.items()
-                    if 'City' in x and y.state_id == self.id]
+            '''
+            Returns a list of city objects with a state_id matching state.id
+            '''
+            from models import storage
+            return [y for x, y in storage.all(City).items()
+                    if y.state_id == self.id]

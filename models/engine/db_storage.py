c225f16a6a21a85a62160f3d5c9663c0095ffb60
StaciAF <aaen.it19@gmail.com>
2020-08-12 22:52:40 +0000

    added structure and some functionality to tasks 7-10, ongoing debugging

---
+++ models/engine/db_storage.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+ '''Defines a new engine using a sql db'''
+
+
+ class DBStorage:
+     '''Contains the methods used in this engine'''
+     __engine = None
+     __session = None
+
+     def __init__(self):
+         '''Initializes the engine'''
+         from sqlalchemy import create_engine
+         from os import getenv
+         user = getenv('HBNB_MYSQL_USER')
+         pw = getenv('HBNB_MYSQL_PWD')
+         host = getenv('HBNB_MYSQL_HOST')
+         db = getenv('HBNB_MYSQL_DB')
+         self.__engine = create_engine('mysql+mysqldb://' + user + ':' + pw +
+                                       '@' + host + ':3306/' + db,
+                                       pool_pre_ping=True)
+         conc = self.__engine.connect()
+         if getenv('HBNB_ENV') == 'test':
+             conc.drop_all(checkfirst=False)
+
+     def all(self, cls=None):
+         '''query on the current database session all objects depending
+         on the class name'''
+         from models.state import State
+         from models.city import City
+         if cls is None:
+             return self.__session.query(State, City).filter(
+                 State.id == City.state_id).all()
+         else:
+             classes = {"State": State, "City": City}
+             print("BUGCHECK")
+             d_list = (self.__session.query(classes[cls]).all())
+             return ({obj.to_dict()['__class__'] + '.' + obj.id: obj for
+                      obj in d_list})
+
+     def new(self, obj):
+         '''Adds an object to the database'''
+         self.__session.add(obj)
+         self.save()
+         self.reload()
+
+     def save(self):
+         '''commit all changes of the current database session'''
+         self.__session.commit()
+
+     def delete(self, obj=None):
+         '''delete from the current database session obj if not None'''
+         if obj is None:
+             return
+         self.__session.delete(obj)
+         self.save()
+
+     def reload(self):
+         '''create all tables in the database'''
+         from sqlalchemy.orm import sessionmaker
+         from sqlalchemy import MetaData
+         from models.base_model import BaseModel, Base
+         from models.amenity import Amenity
+         from models.city import City
+         from models.place import Place
+         from models.review import Review
+         from models.state import State
+         from models.user import User
+         Base.metadata.create_all(self.__engine)
+         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+         Session = scoped_session(session_factory)
+         self.__session = Session()
--- models/place.py
+++ models/place.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey, Float, Table
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+place_amenity = Table('place_amenity', Base.metadata, Column('place_id',
+        String(60), ForeignKey('places.id'), nullable=False, primary_key=True),
+        Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False, primary_key=True))
+
+
+class Place(BaseModel, Base):
+    """ defines class Place with multiple attributes """
+    __tablename__ = 'places'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        review = relationship('Review', cascade='all, delete, delete-orphan', backref='place')
+        amenities = relationship('Amenity', secondary=place_amenity, viewonly=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    else:
+        @property
+        def reviews(self):
+            """ returns a list of Review objects with place_id matching Place.id """
+            from models import storage
+            return self.reviews
+        @property
+        def amenities(self):
+            """ returns a list of Amenity objects with amenity_id matching Amenity.id """
+            from models import storage
+            return self.amenities

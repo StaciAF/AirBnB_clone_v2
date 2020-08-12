e6ac2e33fcce9e05690f22d487167b5ac13c241d
Brett Davis <brett.p.davis101@gmail.com>
2020-08-12 00:41:26 +0000

    Its not crashing anymore, but its also not saving or retrieving. Taking a break, saving progress

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from os import getenv
 
 
-storage = FileStorage()
-storage.reload()
+__all__ = ["base_model", "amenity", "city", "user", "state", "place", "review"]
+if getenv('HBNB_TYPE_STORAGE') != 'db':
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
+else:
+    print("BUGCHECK")
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()

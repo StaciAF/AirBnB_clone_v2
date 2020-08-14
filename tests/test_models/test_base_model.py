87723ec745507767ec8eeaadb4cce63cb860e61a
Brett Davis <brett.p.davis101@gmail.com>
2020-08-14 04:38:32 +0000

    Edits to tests that had errors, removed debug prints

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -29,13 +29,15 @@ class test_basemodel(unittest.TestCase):
 
     def test_default(self):
         """ """
-        if self.value == BaseModel and os.getenv('HBNB_TYPE_STORAGE') == 'db':
-            unittest.SkipTest('BaseModel is not mapped')
-        i = self.value(**{'name': 'testcase'})
+        if self.name == 'BaseModel' and os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            raise unittest.SkipTest('BaseModel is not mapped')
+        i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
         """ """
+        if self.name == 'BaseModel' and os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            raise unittest.SkipTest('BaseModel is not mapped')
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
@@ -43,6 +45,8 @@ class test_basemodel(unittest.TestCase):
 
     def test_kwargs_int(self):
         """ """
+        if self.name == 'BaseModel' and os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            raise unittest.SkipTest('BaseModel is not mapped')
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -51,6 +55,8 @@ class test_basemodel(unittest.TestCase):
 
     def test_save(self):
         """ Testing save """
+        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            raise unittest.SkipTest('BaseModel is not mapped')
         i = self.value()
         i.save()
         key = self.name + "." + i.id
@@ -60,40 +66,54 @@ class test_basemodel(unittest.TestCase):
 
     def test_str(self):
         """ """
+        if self.name == 'BaseModel' and os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            raise unittest.SkipTest('BaseModel is not mapped')
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
         """ """
+        if self.name == 'BaseModel' and os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            raise unittest.SkipTest('BaseModel is not mapped')
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
     def test_kwargs_none(self):
         """ """
+        if self.name == 'BaseModel' and os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            raise unittest.SkipTest('BaseModel is not mapped')
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
     def test_kwargs_one(self):
         """ """
+        if self.name == 'BaseModel' and os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            raise unittest.SkipTest('BaseModel is not mapped')
         n = {'Name': 'test'}
         with self.assertRaises(KeyError):
             new = self.value(**n)
 
     def test_id(self):
         """ """
+        if self.name == 'BaseModel' and os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            raise unittest.SkipTest('BaseModel is not mapped')
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
         """ """
+        if self.name == 'BaseModel' and os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            raise unittest.SkipTest('BaseModel is not mapped')
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_updated_at(self):
         """ """
+        if self.name == 'BaseModel' and os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            raise unittest.SkipTest('BaseModel is not mapped')
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -16,14 +16,14 @@ class test_review(test_basemodel):
     def test_place_id(self):
         """ """
         new = self.value()
-        self.assertTrue(hasattr(new, place_id))
+        self.assertTrue(hasattr(new, 'place_id'))
 
     def test_user_id(self):
         """ """
         new = self.value()
-        self.assertTrue(hasattr(new, user_id))
+        self.assertTrue(hasattr(new, 'user_id'))
 
     def test_text(self):
         """ """
         new = self.value()
-        self.assertTrue(hasattr(new, text))
+        self.assertTrue(hasattr(new, 'text'))

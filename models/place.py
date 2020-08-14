9d6df98c3c6176759888d53d7e0307afbc4e5216
StaciAF <aaen.it19@gmail.com>
2020-08-14 03:55:45 +0000

    missing comma in place_amenity table

--- models/place.py
+++ models/place.py
@@ -9,7 +9,7 @@ from os import getenv
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60),
                              ForeignKey('places.id'),
-                             nullable=False, primary_key=True)
+                             nullable=False, primary_key=True),
                       Column('amenity_id', String(60),
                              ForeignKey('amenities.id'),
                              nullable=False, primary_key=True))

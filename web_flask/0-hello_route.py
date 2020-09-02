09421e8d4662d387db6e63d900441c83104c863f
StaciAF <aaen.it19@gmail.com>
2020-09-02 00:14:49 +0000

    adds script with corrected display, removed comma

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def say_hello():
-    return "Hello, HBNB!"
+    """ function bound to route """
+    return "Hello HBNB!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
